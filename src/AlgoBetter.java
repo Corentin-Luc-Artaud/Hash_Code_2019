3d78c86c6671cc8daf9549b3cb2e39cf7e6855ee
Mathieu Paillart <mathieu.paillart@etu.univ-cotedazur.fr>
2019-02-28 22:04:48 +0100

    Runner

--- src/AlgoBetter.java
+++ src/AlgoBetter.java
@@ -1,7 +1,5 @@
 import java.util.Collections;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 public class AlgoBetter {
     List<Slide> slides;
@@ -14,20 +12,15 @@ public class AlgoBetter {
     public List<Slide> process() {
         for (int i = 0; i < slides.size() - 1; i++) {
             Slide me = slides.get(i);
-            int jmax = (i+100);
+            int jmax = (i + 600);
             int scoremax = 0;
             int index = i + 1;
-            for (int j = i + 1; j < (jmax<=slides.size()?jmax:slides.size()); j++) {
+            for (int j = i + 1; j < (jmax <= slides.size() ? jmax : slides.size()); j++) {
                 int scoreactuel = me.computeScore(slides.get(j));
-                if(scoremax<scoreactuel){
-                    scoremax=scoreactuel;
+                if (scoremax < scoreactuel) {
+                    scoremax = scoreactuel;
                     index = j;
                 }
-                if(me.candidate(slides.get(j))){
-                    index = j;
-                    break;
-                }
-
             }
             Collections.swap(slides, i + 1, index);
         }
